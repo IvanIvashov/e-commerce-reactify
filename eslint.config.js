61204ecd15deac6d095544df77f11a28352b1954
Andrey Baluhatyy <anbaluxes@gmail.com>
2026-01-15 04:04:20 +0300

    add linter + prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,29 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import prettierConfig from 'eslint-config-prettier'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import { globalIgnores } from 'eslint/config'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default tseslint.config([
+    globalIgnores(['dist']),
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    reactHooks.configs['recommended-latest'],
+    reactRefresh.configs.vite,
+    {
+        files: ['**/*.{ts,tsx}'],
+        rules: {
+            'no-console': 'warn',
+            eqeqeq: 'warn',
+            curly: 'warn',
+            'no-else-return': 'warn',
+        },
+        languageOptions: {
+            ecmaVersion: 'latest',
+            globals: globals.browser,
+        },
     },
-  },
+    prettierConfig,
 ])
