f2c047d1689735a0575bf9ccc3ab4028ce30ae13
Andrey Baluhatyy <anbaluxes@gmail.com>
2026-01-15 23:26:04 +0300

    add request products

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,33 @@
+import { useEffect, useState } from 'react'
+
+interface Product {
+    id: number
+    title: string
+    price: number
+    description: string
+    category: string
+    image: string
+}
+
 function App() {
+    const [products, setProducts] = useState<Product[]>([])
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await fetch('https://fakestoreapi.com/products')
+                if (!response.ok) {
+                    throw new Error('Failed to fetch products')
+                }
+                const data: Product[] = await response.json()
+                setProducts(data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        fetchProducts()
+    }, [])
+
     return <></>
 }
 
