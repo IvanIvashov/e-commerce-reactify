4e0875fec60e4183260fe7e9cd2b429047526a6b
Ivan Ivashov <IvashovIA@ya.ru>
2026-01-16 18:45:45 +0300

    edit header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Header } from './components/Header'
+import { Container, Paper, TextField, Button, Typography, Box, Alert } from '@mui/material'
+
 function App() {
-    return <></>
+    return (
+        <>
+            <Header />
+            <Button variant="contained">Contained</Button>
+            <Button variant="contained" disabled>
+                Disabled
+            </Button>
+            <Button variant="contained" href="#contained-buttons">
+                Link
+            </Button>
+        </>
+    )
 }
 
 export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,3 @@
+export function Header() {
+    return <h1>1</h1>
+}
