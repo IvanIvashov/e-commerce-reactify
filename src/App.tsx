820e1a272f91496935d55672a35a4bd4fdb55bc6
Andrey Baluhatyy <anbaluxes@gmail.com>
2026-01-17 15:31:59 +0300

    Checking the received item with the API and refactoring state

--- src/App.tsx
+++ src/App.tsx
@@ -13,56 +13,84 @@ export interface Product {
     image: string
 }
 
+type ProductState =
+    | { state: 'loading' }
+    | { state: 'success'; products: Product[] }
+    | { state: 'error'; message: string }
+
+function isProduct(data: unknown): data is Product[] {
+    return (
+        Array.isArray(data) &&
+        data.every((item) => {
+            typeof item === 'object' &&
+                item !== null &&
+                'id' in item &&
+                'title' in item &&
+                'price' in item &&
+                'description' in item &&
+                'category' in item &&
+                'image' in item
+        })
+    )
+}
+
 function App() {
-    const [products, setProducts] = useState<Product[]>([])
-    const [isLoading, setIsLoading] = useState(false)
-    const [errors, setErrors] = useState<string | null>(null)
+    const [products, setProducts] = useState<ProductState>({ state: 'loading' })
 
     useEffect(() => {
         const fetchProducts = async () => {
             try {
-                setIsLoading(true)
                 const response = await fetch('https://fakestoreapi.com/products')
                 if (!response.ok) {
                     throw new Error(`Failed to fetch products: ${response.status}`)
                 }
-                const data: Product[] = await response.json()
-                setProducts(data)
+                const data: unknown = await response.json()
+                if (!isProduct(data)) {
+                    throw new Error('Error API Response')
+                }
+                setProducts({ state: 'success', products: data })
             } catch (error) {
-                const errorMessage =
-                    error instanceof Error
-                        ? error.message
-                        : 'An unknown error occurred while loading the products'
-                setErrors(errorMessage)
-            } finally {
-                setIsLoading(false)
+                setProducts({
+                    state: 'error',
+                    message:
+                        error instanceof Error
+                            ? error.message
+                            : 'An unknown error occurred while loading the products',
+                })
             }
         }
         fetchProducts()
     }, [])
 
-    if (errors) return <p>Ошибка: {errors}</p>
-    if (isLoading) return <p>Загрузка...</p>
+    switch (products.state) {
+        case 'loading':
+            return <p>Загрузка...</p>
+        case 'error':
+            return <p>Ошибка: {products.message}</p>
+        case 'success':
+            return (
+                <>
+                    <Header />
+                    {products.products.map(({ id, title, price, category, description, image }) => (
+                        <ProductCard
+                            key={id}
+                            product={{
+                                id,
+                                title,
+                                price,
+                                description,
+                                category,
+                                image,
+                            }}
+                            oldPrice={129.99}
+                        />
+                    ))}
+                </>
+            )
 
-    return (
-        <>
-            <Header />
-            {products.map(({ id, title, price, category, description, image }) => (
-                <ProductCard
-                    key={id}
-                    product={{
-                        id,
-                        title,
-                        price,
-                        description,
-                        category,
-                        image,
-                    }}
-                    oldPrice={129.99}
-                />
-            ))}
-        </>
-    )
+        default:
+            return null
+    }
 }
 
 export default App
